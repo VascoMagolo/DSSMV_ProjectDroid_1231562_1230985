d2b995cd36c5eb5b836a5421d8c0018c6d090ef3
Vasco Magolo <vascomagolo@gmail.com>
2025-10-24 16:33:20 +0100

    Inicial commit

---
+++ app/src/test/java/rttc/dssmv_projectdroid_1231562_1230985/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package rttc.dssmv_projectdroid_1231562_1230985;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DSSMV_ProjectDroid_1231562_1230985"
+include(":app")
+ 
