19d23443cffccb533cc653b69836edc78b4adcbc
franc <franciscolemossilva@gmail.com>
2025-10-31 02:52:16 +0000

    Started register process

--- app/src/main/java/rttc/dssmv_projectdroid_1231562_1230985/view/RegisterActivity.java
+++ app/src/main/java/rttc/dssmv_projectdroid_1231562_1230985/view/RegisterActivity.java
@@ -1,14 +1,55 @@
 package rttc.dssmv_projectdroid_1231562_1230985.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import rttc.dssmv_projectdroid_1231562_1230985.R;
+import rttc.dssmv_projectdroid_1231562_1230985.controller.RegisterController;
 
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText edtName, edtEmail, edtPassword;
+    private Button btnRegister;
+    private RegisterController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_register);
+
+        controller = new RegisterController();
+
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnRegister = findViewById(R.id.btnCreate);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                controller.registerUser(name, email, password);
+            }
+        });
+    }
+
+    public void showSuccessMessage() {
+        Toast.makeText(this, "Conta criada com sucesso", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showErrorMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToHome() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
