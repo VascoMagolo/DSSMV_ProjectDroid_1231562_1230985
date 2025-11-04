f010dedbcc454323104e9f1476a2fc9354fe6adc
Vasco Magolo <vascomagolo@gmail.com>
2025-11-04 20:41:28 +0000

    Added ConversationHistoryActivity, SessionManager and modified LoginActivity

---
+++ app/src/main/java/rttc/dssmv_projectdroid_1231562_1230985/view/ConversationHistoryActivity.java
@@ -0,0 +1,17 @@
+package rttc.dssmv_projectdroid_1231562_1230985.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import rttc.dssmv_projectdroid_1231562_1230985.R;
+import rttc.dssmv_projectdroid_1231562_1230985.view.fragments.AccountFragment;
+import rttc.dssmv_projectdroid_1231562_1230985.view.fragments.ImageFragment;
+
+public class ConversationHistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_conversation_history);
+    }
+}
